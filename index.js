6c0f5fee2a40625df654330502b8160e63c5f85a
Md. Jobayer Hossain <jobayerhossain298@gmail.com>
2023-06-07 21:51:14 +0600

    connected to dv

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 require("dotenv").config();
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.je2pvxf.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("summerCamp").collection("users")
+
+
+    app.put("/users/:email" , async(req, res) =>{
+      const email = req.params.email
+      const user = req.body
+      const query = {email: email}
+      const options = {upsert: true}
+      const updateDoc = {
+        $set : user 
+      }
+      const result = await userCollection.updateOne(query, updateDoc, options)
+      console.log(result);
+      res.send(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
